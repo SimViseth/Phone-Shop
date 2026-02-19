7243c02048ffae7d0484a531de1bed15adcd9712
viseth <simviseth509@gmail.com>
2026-02-19 21:30:34 +0700

    sale

--- src/main/java/com/example/phoneshop/service/serviceImplement/SaleServiceImpl.java
+++ src/main/java/com/example/phoneshop/service/serviceImplement/SaleServiceImpl.java
@@ -1,13 +1,16 @@
 package com.example.phoneshop.service.serviceImplement;
 
 import com.example.phoneshop.dto.SaleDTO;
-import com.example.phoneshop.dto.product.ProductImportDTO;
-import com.example.phoneshop.dto.product.ProductSoldDTO;
 import com.example.phoneshop.entity.Product;
+import com.example.phoneshop.entity.Sale;
+import com.example.phoneshop.exception.ApiException;
+import com.example.phoneshop.repository.ProductRepository;
+import com.example.phoneshop.repository.SaleDetailRepository;
 import com.example.phoneshop.repository.SaleRepository;
 import com.example.phoneshop.service.ProductService;
 import com.example.phoneshop.service.SaleService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -17,13 +20,21 @@ import java.util.List;
 public class SaleServiceImpl implements SaleService {
     private final SaleRepository saleRepository;
     private final ProductService productService;
+    private final ProductRepository productRepository;
+    private final SaleDetailRepository saleDetailRepository;
 
     @Override
     public void sellProduct(SaleDTO saleDTO) {
         // validate
-
+        validate(saleDTO);
 
         // save
+    }
+
+    private void saveSale(SaleDTO saleDTO) {
+        Sale sale = new Sale();
+        sale.setSoldDate(saleDTO.getSaleDate());
+        saleRepository.save(sale);
     }
 
     // validate
@@ -37,14 +48,18 @@ public class SaleServiceImpl implements SaleService {
                     .toList();
          */
 
-        // Option 2
+        /* Option 2
         saleDTO.getProducts().stream()
                 .map(ProductSoldDTO::getProductId)
                 .forEach(productService::getById); // = forEach(productId -> productService.getById(productId));
+        */
 
-
-        // ------- VALIDATE STOCK ------------
-
+        saleDTO.getProducts().forEach(ps -> {
+            Product product = productService.getById(ps.getProductId());
+            if (product.getAvailableUnit() < ps.getNumberOfUnit()) {
+                throw new ApiException(HttpStatus.BAD_REQUEST, "Product [%s] is not enough in stock");
+            }
+        });
 
     }
 }
