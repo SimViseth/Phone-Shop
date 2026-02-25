362227b7c10787ac0189a51d10794855e8292bd5
viseth <simviseth509@gmail.com>
2026-02-25 21:37:19 +0700

    get report

--- src/main/java/com/example/phoneshop/repository/SaleRepository.java
+++ src/main/java/com/example/phoneshop/repository/SaleRepository.java
@@ -9,17 +9,18 @@ import java.time.LocalDate;
 import java.util.List;
 
 public interface SaleRepository extends JpaRepository<Sale, Long> {
-
-    @Query(value = "SELECT\n" +
-            "    p.product_id AS productId,\n" +
-            "    p.product_name AS productName,\n" +
-            "    SUM(sd.unit) AS unit,\n" +
-            "    SUM(sd.unit * sd.sold_amount) AS totalAmount\n" +
-            "FROM sale_details sd\n" +
-            "INNER JOIN sales s ON sd.sale_id = s.sale_id\n" +
-            "INNER JOIN products p ON p.product_id = sd.product_id\n" +
-            "WHERE date(s.sold_date) >= :startDate AND date(s.sold_date) <= :endDate\n" +
-            "GROUP BY p.product_id, p.product_name",
+    @Query(value = """
+            SELECT
+                p.product_id AS productId,
+                p.product_name AS productName,
+                SUM(sd.unit) AS unit,
+                SUM(sd.unit * sd.sold_amount) AS totalAmount
+            FROM sale_details sd
+            INNER JOIN sales s ON sd.sale_id = s.sale_id
+            INNER JOIN products p ON p.product_id = sd.product_id
+            WHERE date(s.sold_date) >= :startDate AND date(s.sold_date) <= :endDate
+            GROUP BY p.product_id, p.product_name
+            """,
             nativeQuery = true)
     List<ProductSoldProjection> findProductSold(LocalDate startDate, LocalDate endDate);
 }
